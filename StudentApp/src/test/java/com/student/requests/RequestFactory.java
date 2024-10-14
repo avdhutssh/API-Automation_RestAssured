d089f4634e44cf4d5a4d3714d0ad5e3b66151846
avdhutssh <avdhut.ssh@gmail.com>
2024-10-14 19:13:50 +0530

    Created RequestFactory

--- StudentApp/src/test/java/com/student/requests/RequestFactory.java
+++ StudentApp/src/test/java/com/student/requests/RequestFactory.java
@@ -1,4 +1,73 @@
 package com.student.requests;
 
-public class RequestFactory {
+import com.student.pojo.StudentClass;
+import com.student.tests.BaseTest;
+import io.qameta.allure.Step;
+import io.restassured.response.Response;
+
+import java.util.List;
+
+public class RequestFactory extends BaseTest {
+
+    RestClient restClient = new RestClient();
+
+    @Step("Getting all students information from the DB")
+    public Response getAllStudents() {
+
+        return restClient.doGetRequest("/list");
+    }
+
+    @Step("Creating a new student : {0} , {1} , {2} ,{3} , {4}")
+    public Response createNewStudent(String url, String firstName, String lastName, String email,
+                                     String programme, List<String> courses) {
+
+        StudentClass studentReqPayload = new StudentClass();
+        studentReqPayload.setFirstName(firstName);
+        studentReqPayload.setLastName(lastName);
+        studentReqPayload.setEmail(email);
+        studentReqPayload.setProgramme(programme);
+        studentReqPayload.setCourses(courses);
+
+        return restClient.doPostRequest(url, studentReqPayload);
+    }
+
+
+    @Step("Updating student information with studentId: {0}, firstName: {1}, lastName: {2}, email: {3}, programme: {4}, courses: {5}")
+    public Response updateStudent(int studentid, String firstName,
+                                  String lastName, String email, String programme,
+                                  List<String> courses) {
+
+        StudentClass student = new StudentClass();
+        student.setFirstName(firstName);
+        student.setLastName(lastName);
+        student.setEmail(email);
+        student.setProgramme(programme);
+        student.setCourses(courses);
+
+        return restClient.doPutRequest("/" + studentid, student);
+
+    }
+
+    @Step("Updating student information with email: {0}")
+    public Response updateStudentEmailAddress(String url, String email) {
+
+        StudentClass student = new StudentClass();
+        student.setEmail(email);
+        return restClient.doPatchRequest(url, student);
+
+    }
+
+    @Step("Deleting student info with Id: {0}")
+    public Response deleteStudent(int studentId) {
+
+        return restClient.doDeleteRequest("/" + studentId);
+
+    }
+
+    public Response getStudentById(int studentId) {
+
+        return restClient.doGetRequest("/" + studentId);
+
+
+    }
 }
