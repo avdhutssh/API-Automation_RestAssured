66f7b6ebea5439b77313042aa1bebeb5276c738f
avdhutssh <avdhut.ssh@gmail.com>
2024-10-14 19:14:17 +0530

    Created CRUD_Test

--- StudentApp/src/test/java/com/student/tests/CRUD_Test.java
+++ StudentApp/src/test/java/com/student/tests/CRUD_Test.java
@@ -1,41 +1,50 @@
 package com.student.tests;
 
+import com.student.requests.RequestFactory;
+import com.student.specs.SpecificationFactory;
+import com.student.util.AssertionUtils;
+import io.qameta.allure.Feature;
+import io.qameta.allure.Story;
+import io.restassured.response.ValidatableResponse;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import static io.restassured.RestAssured.given;
+public class CRUD_Test extends BaseTest {
 
-public class CRUD_Test extends BaseTest{
+    RequestFactory request = new RequestFactory();
 
+    @Story("This is a CRUD testing story")
+    @DisplayName("This a test to get all students from the database")
+    @Feature("This a test to get all students from the database")
     @Test
-    public void getAllStudentsInfo(){
+    public void getAllStudentsInfo() {
 
-       String response =  given()
-                          .when()
-                          .get("/list")
-                          .then()
-                          .statusCode(200)
-                          .extract()
-                          .asString();
-        System.out.println(response);
-    }
+        ValidatableResponse response = request.getAllStudents()
+                .then()
+                .spec(SpecificationFactory.getGenericResponseSpec());
 
-    @Test
-    public void createNewStudent(){
-
-    }
-
-    @Test
-    public void updateStudentInfo(){
+        AssertionUtils.verifyStatusCode(response, 200);
+        AssertionUtils.verifyResponsePayload(response, "[0].firstName", "Vernon");
 
     }
 
     @Test
-    public void updateStudentEmail(){
+    public void createNewStudent() {
 
     }
 
     @Test
-    public void deleteStudentFromDB(){
+    public void updateStudentInfo() {
+
+    }
+
+    @Test
+    public void updateStudentEmail() {
+
+    }
+
+    @Test
+    public void deleteStudentFromDB() {
 
     }
 }
