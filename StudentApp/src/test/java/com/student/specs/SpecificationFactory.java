87172672ae90e8ccd896f2e5385d9222f73f2b2a
avdhutssh <avdhut.ssh@gmail.com>
2024-10-14 19:14:08 +0530

    Created SpecificationFactory

--- StudentApp/src/test/java/com/student/specs/SpecificationFactory.java
+++ StudentApp/src/test/java/com/student/specs/SpecificationFactory.java
@@ -1,4 +1,53 @@
 package com.student.specs;
 
-public class SpecificationFactory {
+import com.student.tests.BaseTest;
+import io.qameta.allure.restassured.AllureRestAssured;
+import io.restassured.builder.RequestSpecBuilder;
+import io.restassured.builder.ResponseSpecBuilder;
+import io.restassured.specification.RequestSpecification;
+import io.restassured.specification.ResponseSpecification;
+
+import java.util.concurrent.TimeUnit;
+
+import static org.hamcrest.Matchers.lessThan;
+
+public class SpecificationFactory extends BaseTest {
+
+//    public static ResponseSpecification verifyStatusCodeAndGiveStringResponse(int StatusCode) {
+//
+//        ResponseSpecBuilder responseSpecBuilder = new ResponseSpecBuilder();
+//        responseSpecBuilder.expectHeader("Content-Type", "application/json;charset=UTF-8");
+//        responseSpecBuilder.expectHeader("Transfer-Encoding", "chunked");
+//        responseSpecBuilder.expectStatusCode(StatusCode);
+//
+//        ResponseSpecification responseSpecification = responseSpecBuilder.build();
+//        return responseSpecification;
+//    }
+
+
+    public static ResponseSpecification getGenericResponseSpec() {
+
+        ResponseSpecBuilder responseSpecBuilder = new ResponseSpecBuilder();
+        responseSpecBuilder.expectHeader("Content-Type", "application/json;charset=UTF-8");
+        responseSpecBuilder.expectHeader("Transfer-Encoding", "chunked");
+        responseSpecBuilder.expectResponseTime(lessThan(5L), TimeUnit.SECONDS);
+
+        ResponseSpecification responseSpecification = responseSpecBuilder.build();
+        return responseSpecification;
+    }
+
+    public static synchronized RequestSpecification logPayloadResponseInfo() {
+        RequestSpecBuilder logBuilder = new RequestSpecBuilder();
+
+
+        if (prop.getProperty("log").equals("ENABLE")) {
+
+            logBuilder.addFilter(new AllureRestAssured());
+
+        }
+
+        RequestSpecification logSpecification = logBuilder.build();
+        return logSpecification;
+
+    }
 }
