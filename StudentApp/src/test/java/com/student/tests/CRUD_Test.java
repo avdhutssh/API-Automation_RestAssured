8d3e76bc0a0846811bc3c015da341c8d4b108698
avdhutssh <avdhut.ssh@gmail.com>
2024-10-15 17:22:14 +0530

    Refactor version of CRUD test

--- StudentApp/src/test/java/com/student/tests/CRUD_Test.java
+++ StudentApp/src/test/java/com/student/tests/CRUD_Test.java
@@ -8,6 +8,7 @@ import com.student.util.TestUtils;
 import io.qameta.allure.Feature;
 import io.qameta.allure.Story;
 import io.restassured.response.ValidatableResponse;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
@@ -17,8 +18,15 @@ import java.util.List;
 
 public class CRUD_Test extends BaseTest {
 
-    RequestFactory request = new RequestFactory();
-    String lastID;
+    private RequestFactory request;
+    private String lastID;
+    private Faker randomData;
+
+    @BeforeEach
+    public void setUp() {
+        request = new RequestFactory();
+        randomData = new Faker();
+    }
 
     @Tag("Smoke")
     @Story("This is a CRUD testing story")
@@ -26,33 +34,26 @@ public class CRUD_Test extends BaseTest {
     @Feature("This a test to get all students from the database")
     @Test
     public void _01_getAllStudentsInfo() {
-
         ValidatableResponse response = request.getAllStudents()
                 .then()
                 .spec(SpecificationFactory.getGenericResponseSpec());
 
         AssertionUtils.verifyStatusCode(response, 200);
         AssertionUtils.verifyResponsePayload(response, "[0].firstName", "Vernon");
-
     }
 
     @Story("This is a CRUD testing story")
     @DisplayName("Test to create & verify a new student")
     @Feature("Test to create & verify a new student")
-    @Tag("Regression,Smoke")
+    @Tag("Regression")
+    @Tag("Smoke")
     @Test
     public void _02_createNewStudent() {
-        Faker randomData = new Faker();
-
         String firstName = randomData.name().firstName();
         String lastName = randomData.name().lastName();
         String email = "testAvPost_" + randomData.internet().emailAddress();
-
         String programme = "ComputerScience";
-
-        List<String> courses = new ArrayList<String>();
-        courses.add("Java");
-        courses.add("Rest Assured");
+        List<String> courses = createCourseList();
 
         ValidatableResponse response = request.createNewStudent("", firstName, lastName, email, programme, courses)
                 .then()
@@ -61,8 +62,8 @@ public class CRUD_Test extends BaseTest {
         AssertionUtils.verifyStatusCode(response, 201);
         AssertionUtils.verifyResponsePayload(response, "msg", "Student added");
 
-        this.getLastStudentID();
-        System.out.println("Newly created student ID is: " + lastID);
+        fetchLastStudentId();
+        logNewStudentID(lastID);
     }
 
     @Tag("Regression")
@@ -71,16 +72,12 @@ public class CRUD_Test extends BaseTest {
     @Feature("Test to update an existing student")
     @Test
     public void _03_updateStudentInfo() {
-
-        this.getLastStudentID();
-        Faker randomData = new Faker();
+        fetchLastStudentId();
         String firstName = randomData.name().firstName();
         String lastName = randomData.name().lastName();
         String email = "testAvPut_" + randomData.internet().emailAddress();
         String programme = "ComputerScience";
-        List<String> courses = new ArrayList<>();
-        courses.add("Java");
-        courses.add("Rest Assured");
+        List<String> courses = createCourseList();
 
         ValidatableResponse response = request.updateStudent(lastID, firstName, lastName, email, programme, courses)
                 .then()
@@ -88,7 +85,6 @@ public class CRUD_Test extends BaseTest {
 
         AssertionUtils.verifyStatusCode(response, 200);
         AssertionUtils.verifyResponsePayload(response, "msg", "Student Updated");
-
     }
 
     @Tag("Regression")
@@ -97,8 +93,7 @@ public class CRUD_Test extends BaseTest {
     @Feature("Test to update student email")
     @Test
     public void _04_updateStudentEmail() {
-        this.getLastStudentID();
-        Faker randomData = new Faker();
+        fetchLastStudentId();
         String email = "testAvPatch_" + randomData.internet().emailAddress();
 
         ValidatableResponse response = request.updateStudentEmailAddress(lastID, email)
@@ -107,33 +102,43 @@ public class CRUD_Test extends BaseTest {
 
         AssertionUtils.verifyStatusCode(response, 200);
         AssertionUtils.verifyResponsePayload(response, "msg", "Updated");
-
     }
 
     @Story("This is a CRUD testing story")
     @DisplayName("Test to delete student")
     @Feature("Test to delete student")
-    @Tag("Regression,Smoke")
+    @Tag("Regression")
+    @Tag("Smoke")
     @Test
     public void _05_deleteStudentFromDB() {
-
-        this.getLastStudentID();
-        System.out.println("last Id is: " + lastID);
+        fetchLastStudentId();
+        logDeletingStudent(lastID);
         ValidatableResponse response = request.deleteStudent(lastID)
                 .then();
 
         AssertionUtils.verifyStatusCode(response, 204);
-
     }
 
-    public String getLastStudentID() {
+    private void fetchLastStudentId() {
         ValidatableResponse response = request.getAllStudents()
                 .then()
                 .spec(SpecificationFactory.getGenericResponseSpec());
         AssertionUtils.verifyStatusCode(response, 200);
-        String lastId = TestUtils.getJsonPayloadValue(response, "[-1].id");
-        this.lastID = lastId;
-        return lastID;
+        this.lastID = TestUtils.getJsonPayloadValue(response, "[-1].id");
+    }
 
+    private List<String> createCourseList() {
+        List<String> courses = new ArrayList<>();
+        courses.add("Java");
+        courses.add("Rest Assured");
+        return courses;
+    }
+
+    private void logNewStudentID(String lastID) {
+        System.out.println("Newly created student ID is: " + lastID);
+    }
+
+    private void logDeletingStudent(String lastID) {
+        System.out.println("Deleting student with ID: " + lastID);
     }
 }
