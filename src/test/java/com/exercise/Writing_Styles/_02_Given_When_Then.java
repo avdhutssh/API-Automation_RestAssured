c2462c9c4a1ca3caacec3b2e49436a69d0e2ca8b
avdhutssh <avdhut.ssh@gmail.com>
2024-12-21 12:10:43 +0530

    exercise -> _02_Given_When_Then -> Validate response with Request headers and parameters

--- src/test/java/com/exercise/Writing_Styles/_02_Given_When_Then.java
+++ src/test/java/com/exercise/Writing_Styles/_02_Given_When_Then.java
@@ -1,6 +1,7 @@
 package com.exercise.Writing_Styles;
 
 import io.restassured.RestAssured;
+import io.restassured.http.ContentType;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -28,8 +29,30 @@ public class _02_Given_When_Then {
                 .statusCode(200)
                 .log()
                 .all()
+                .headers("Content-Type", "application/json;charset=UTF-8",
+                        "Transfer-Encoding", "chunked")
+                .time(lessThan(3000L))
+                .contentType(ContentType.JSON)
                 .body("[0].firstName", equalTo("Vernon"))
+                .body("findAll{it.courses.contains('Accounting')}.firstName", hasItems("Vernon"))
                 .body("[0].courses", hasItems("Accounting", "Statistics"))
                 .body("size()", greaterThan(1));
     }
+
+    @DisplayName("Validate response with Request headers and parameters")
+    @Test
+    void _02_validateWithRequestHeadersAndParams() {
+        RestAssured.given()
+                .log()
+                .all()
+                .header("Content-Type", "application/json")
+                .param("programme", "Financial Analysis")
+                .when()
+                .get("/list")
+                .then()
+                .log().all()
+                .statusCode(200)
+                .body("findAll{it.programme=='Financial Analysis'}.size()", equalTo(10));
+    }
+
 }
