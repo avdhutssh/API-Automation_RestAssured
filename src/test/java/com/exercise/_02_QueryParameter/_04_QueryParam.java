64407f29de2f1c2f876c2c09b5cc1f1f0b2b517c
avdhutssh <avdhut.ssh@gmail.com>
2025-01-04 21:49:00 +0530

    exercise -> Query Parameter -> one by one

---
+++ src/test/java/com/exercise/_02_QueryParameter/_04_QueryParam.java
@@ -0,0 +1,35 @@
+package com.exercise._02_QueryParameter;
+
+import io.restassured.RestAssured;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.hamcrest.Matchers.*;
+import static io.restassured.RestAssured.*;
+
+public class _04_QueryParam {
+
+    @BeforeAll
+    public static void setUp() {
+        RestAssured.baseURI = "http://localhost:8085";
+        RestAssured.basePath = "/student";
+    }
+
+    @DisplayName("Getting student information using Query parameter one by one")
+    @Test
+    public void _01_QP_OneByOne() {
+        given()
+                .queryParam("programme", "Computer Science")
+                .queryParam("limit", 1)
+                .log()
+                .all()
+                .when()
+                .get("/list")
+                .then()
+                .log()
+                .all()
+                .statusCode(200)
+                .body("size()", equalTo(1));
+    }
+}
--- src/test/java/com/exercise/_04_QueryParameter.java
+++
@@ -1,5 +0,0 @@
-package com.exercise;
-
-public class _04_QueryParameter {
-    
-}
