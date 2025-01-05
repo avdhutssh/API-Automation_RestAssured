d433a97e208f02deb9a8f0529c105c6ea0ee2c94
avdhutssh <avdhut.ssh@gmail.com>
2025-01-05 11:38:48 +0530

    exercise -> Query Parameter -> Map

--- src/test/java/com/exercise/_02_QueryParameter/_04_QueryParam.java
+++ src/test/java/com/exercise/_02_QueryParameter/_04_QueryParam.java
@@ -5,6 +5,9 @@ import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import static org.hamcrest.Matchers.*;
 import static io.restassured.RestAssured.*;
 
@@ -38,6 +41,26 @@ public class _04_QueryParam {
     public void _02_QP_AllInOne() {
         given()
                 .queryParams("programme", "Computer Science", "limit", 1)
+                .log()
+                .all()
+                .when()
+                .get("/list")
+                .then()
+                .log()
+                .all()
+                .statusCode(200)
+                .body("size()", equalTo(1));
+    }
+
+
+    @DisplayName("Getting student information using Query parameter Map")
+    @Test
+    public void _03_QP_Map() {
+        Map<String, Object> requestQP = new HashMap<>();
+        requestQP.put("programme", "Computer Science");
+        requestQP.put("limit", 1);
+        given()
+                .queryParams(requestQP)
                 .log()
                 .all()
                 .when()
