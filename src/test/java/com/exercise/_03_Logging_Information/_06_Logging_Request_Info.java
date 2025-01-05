1c97863f26c2cccae21ff99b4b5cfe72a99eff87
avdhutssh <avdhut.ssh@gmail.com>
2025-01-05 19:35:40 +0530

    exercise -> Logging Request Info -> print out the request body

--- src/test/java/com/exercise/_03_Logging_Information/_06_Logging_Request_Info.java
+++ src/test/java/com/exercise/_03_Logging_Information/_06_Logging_Request_Info.java
@@ -1,12 +1,18 @@
 package com.exercise._03_Logging_Information;
 
+import com.github.javafaker.Faker;
+import com.student.pojo.StudentClass;
 import io.qameta.allure.Description;
 
 import static io.restassured.RestAssured.*;
 
 import io.restassured.RestAssured;
+import io.restassured.http.ContentType;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class _06_Logging_Request_Info {
 
@@ -43,4 +49,36 @@ public class _06_Logging_Request_Info {
                 .then()
                 .statusCode(200);
     }
+
+    @Description("This test will print out the Request body")
+    @Test
+    public void _03_Print_RequestBody() {
+        StudentClass student = new StudentClass();
+        Faker randomData = new Faker();
+
+        List<String> courses = new ArrayList<>();
+        courses.add("Java");
+        courses.add("Rest Assured");
+
+        student.setFirstName(randomData.name().firstName());
+        student.setLastName(randomData.name().lastName());
+        student.setEmail(randomData.internet().emailAddress());
+        student.setProgramme("Computer Science");
+        student.setCourses(courses);
+
+        String response = given()
+                .log()
+                .body()
+                .when()
+                .contentType(ContentType.JSON)
+                .body(student)
+                .post()
+                .then()
+                .statusCode(201)
+                .extract()
+                .asString();
+
+        System.out.println(response);
+    }
+
 }
