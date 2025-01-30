07a58a9cd7dfe5a0476138b5a07acf0aa3669b1d
avdhutssh <avdhut.ssh@gmail.com>
2025-01-30 11:39:43 +0530

    exercise -> XMLPathExample -> new Pet API added

--- src/test/java/com/exercise/_05_Xml/XMLPathExample.java
+++ src/test/java/com/exercise/_05_Xml/XMLPathExample.java
@@ -2,46 +2,98 @@ package com.exercise._05_Xml;
 
 import io.qameta.allure.Description;
 import io.restassured.RestAssured;
-import io.restassured.response.Response;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
-import static io.restassured.RestAssured.*;
-import static org.hamcrest.Matchers.*;
+import static io.restassured.RestAssured.given;
 
 public class XMLPathExample {
+    private static String pet_ID;
 
     @BeforeAll
     public static void init() {
 //        RestAssured.baseURI = "https://api.weather.gov";
-        RestAssured.baseURI = "http://webservices.oorsprong.org/websamples.countryinfo";
-    }
+//        RestAssured.baseURI = "http://webservices.oorsprong.org/websamples.countryinfo";
+        RestAssured.baseURI = "https://petstore.swagger.io";
+        RestAssured.basePath = "/v2";
+        // Create pet and store ID in BeforeClass
+        String xmlBody = "<?xml version=\"1.0\" encoding=\"UTF-8\"?>" +
+                "<Pet>" +
+                "<id>0</id>" +
+                "<Category>" +
+                "<id>0</id>" +
+                "<name>string</name>" +
+                "</Category>" +
+                "<name>doggie</name>" +
+                "<photoUrls>" +
+                "<photoUrl>string</photoUrl>" +
+                "</photoUrls>" +
+                "<tags>" +
+                "<Tag>" +
+                "<id>0</id>" +
+                "<name>string</name>" +
+                "</Tag>" +
+                "</tags>" +
+                "<status>available</status>" +
+                "</Pet>";
 
-    @Description("Verify status code and content type")
-    @Test
-    public void _01_VerifyContentType() {
-        given()
-                .contentType("text/xml")
-                .body("<soapenv:Envelope xmlns:soapenv=\"http://schemas.xmlsoap.org/soap/envelope/\" xmlns:web=\"http://www.oorsprong.org/websamples.countryinfo\">\n" +
-                        "   <soapenv:Header/>\n" +
-                        "   <soapenv:Body>\n" +
-                        "      <web:CapitalCity>\n" +
-                        "         <web:sCountryISOCode>US</web:sCountryISOCode>\n" +
-                        "      </web:CapitalCity>\n" +
-                        "   </soapenv:Body>\n" +
-                        "</soapenv:Envelope>")
+        pet_ID = given()
+                .contentType("application/xml")
+                .accept("application/xml")
+                .body(xmlBody)
                 .when()
-                .post("/CountryInfoService.wso")
+                .post("/pet")
                 .then()
                 .statusCode(200)
                 .log()
                 .all()
-                .contentType(containsString("text/xml"));
+                .extract()
+                .path("Pet.id")
+                .toString();
+
+        System.out.println("Created pet with ID: " + pet_ID);
+    }
+
+    @Description("Verify status code and content type")
+    @Test
+    public void _01_VerifyContentType1() {
+        given()
+                .accept("application/xml")
+                .pathParam("petId", pet_ID)
+                .when()
+                .get("/pet/{petId}")
+                .then()
+                .statusCode(200)
+                .log()
+                .all()
+                .contentType("application/xml");
     }
 
 //    @Description("Verify status code and content type")
 //    @Test
 //    public void _01_VerifyContentType() {
+//        given()
+//                .contentType("text/xml")
+//                .body("<soapenv:Envelope xmlns:soapenv=\"http://schemas.xmlsoap.org/soap/envelope/\" xmlns:web=\"http://www.oorsprong.org/websamples.countryinfo\">\n" +
+//                        "   <soapenv:Header/>\n" +
+//                        "   <soapenv:Body>\n" +
+//                        "      <web:CapitalCity>\n" +
+//                        "         <web:sCountryISOCode>US</web:sCountryISOCode>\n" +
+//                        "      </web:CapitalCity>\n" +
+//                        "   </soapenv:Body>\n" +
+//                        "</soapenv:Envelope>")
+//                .when()
+//                .post("/CountryInfoService.wso")
+//                .then()
+//                .statusCode(200)
+//                .log()
+//                .all()
+//                .contentType(containsString("text/xml"));
+//    }
+//
+//    @Description("Verify status code and content type")
+//    @Test
+//    public void _01_VerifyContentType1() {
 //        given()
 //                .header("Accept", "application/xml")
 //                .when()
@@ -51,7 +103,6 @@ public class XMLPathExample {
 //                .log()
 //                .all();
 ////                .contentType(containsString("application/xml"));
-//
 //    }
 
 }
