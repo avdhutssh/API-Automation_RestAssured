615bb13fd0fbb1c80918c430b84a5ccba90652d4
avdhutssh <avdhut.ssh@gmail.com>
2025-05-03 21:21:32 +0530

    PayPal -> pojo -> Payee

---
+++ src/main/java/com/paypal/pojo/order/Payee.java
@@ -0,0 +1,16 @@
+package com.paypal.pojo.order;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@JsonInclude(JsonInclude.Include.NON_NULL)
+public class Payee {
+    private String email_address;
+
+    private String merchant_id;
+}
