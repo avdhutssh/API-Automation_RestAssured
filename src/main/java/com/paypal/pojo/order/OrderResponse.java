40f61e9421ffb93e7e28849cfee72241c24edf17
avdhutssh <avdhut.ssh@gmail.com>
2025-05-03 21:20:41 +0530

    PayPal -> pojo -> OrderResponse updated

--- src/main/java/com/paypal/pojo/order/OrderResponse.java
+++ src/main/java/com/paypal/pojo/order/OrderResponse.java
@@ -2,22 +2,18 @@ package com.paypal.pojo.order;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonProperty;
-import lombok.AllArgsConstructor;
 import lombok.Data;
-import lombok.NoArgsConstructor;
 
 import java.util.List;
 
 @Data
-@NoArgsConstructor
-@AllArgsConstructor
 @JsonInclude(JsonInclude.Include.NON_NULL)
 public class OrderResponse {
     private String id;
 
     private String status;
 
-    private Links links;
+    private String intent;
 
     @JsonProperty("purchase_units")
     private List<PurchaseUnit> purchaseUnits;
@@ -25,8 +21,5 @@ public class OrderResponse {
     @JsonProperty("create_time")
     private String createTime;
 
-    private String intent;
-
-    @JsonProperty("payment_source")
-    private PaymentSource paymentSource;
+    private List<Link> links;
 }
