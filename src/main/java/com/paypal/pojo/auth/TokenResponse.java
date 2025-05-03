4d822feab2bfd470a2153261bdc468bbd7066df8
avdhutssh <avdhut.ssh@gmail.com>
2025-05-03 17:16:31 +0530

    PayPal -> pojo -> TokenResponse

---
+++ src/main/java/com/paypal/pojo/auth/TokenResponse.java
@@ -0,0 +1,29 @@
+package com.paypal.pojo.auth;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@JsonInclude(JsonInclude.Include.NON_NULL)
+public class TokenResponse {
+    private String scope;
+
+    @JsonProperty("access_token")
+    private String accessToken;
+
+    @JsonProperty("token_type")
+    private String tokenType;
+
+    @JsonProperty("app_id")
+    private String appId;
+
+    @JsonProperty("expires_in")
+    private int expiresIn;
+
+    private String nonce;
+}
